e8b554b3fd3797cfb88dc4f5883f92d5ae7f4cae
gobogit <carlos@gobo.tv>
2017-08-21 15:04:37 +0200

    rebuilding site lun., 21 de ago. de 2017 03:04:37 p. m.

---
+++ js/codigo.js
@@ -0,0 +1,6 @@
+window.onscroll = () => {
+
+  if(this.scrollY <= 120) $("#navbar").removeClass("scroll"); else $("#navbar").addClass("scroll");
+    if(this.scrollY >= 120) $(".nav-item").removeClass("anav"); else $(".nav-item").addClass("anav");
+
+};
