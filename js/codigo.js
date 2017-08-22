8bccf13199cc5305cee7453c3b1e284f21c0313d
Carlos Santos <carlos@gobo.tv>
2017-08-22 19:30:27 +0200

    rebuilding site Tue Aug 22 19:30:27     2017

--- js/codigo.js
+++ js/codigo.js
@@ -1,6 +1,4 @@
 window.onscroll = () => {
-
   if(this.scrollY <= 120) $("#navbar").removeClass("scroll"); else $("#navbar").addClass("scroll");
-    if(this.scrollY >= 120) $(".nav-item").removeClass("anav"); else $(".nav-item").addClass("anav");
-
+  if(this.scrollY >= 120) $(".nav-item").removeClass("anav"); else $(".nav-item").addClass("anav");
 };
