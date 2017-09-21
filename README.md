e493be8fd8efca072be83e7be39894d68c67f6dc
Carlos Santos <carlos@gobo.tv>
2017-09-21 19:58:25 +0200

    post Houdini Displacement

--- README.md
+++
@@ -1,1 +0,0 @@
-# gobogit.github.io
--- js/codigo.js
+++ js/codigo.js
@@ -2,3 +2,12 @@ window.onscroll = () => {
   if(this.scrollY <= 120) $("#navbar").removeClass("scroll"); else $("#navbar").addClass("scroll");
   if(this.scrollY >= 120) $(".nav-item").removeClass("anav"); else $(".nav-item").addClass("anav");
 };
+
+$('#myModal').on('show.bs.modal', function (event) {
+  var click = $(event.relatedTarget) // click that triggered the modal
+  var titulo = click.data('titu') // Extract info from data-* attributes
+  var srca = click.data('srct') // Extract info from data-* attributes
+
+  $('.modal-title').text(titulo)
+  $('.imgFull').attr('src', srca);
+})
